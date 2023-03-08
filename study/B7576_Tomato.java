09e896530338a310c3ee286dcc5d87a83a035dcc
YuSinPark <pus12369@naver.com>
2023-03-08 15:25:42 +0900

    토마토

--- study/B7576_Tomato.java
+++ study/B7576_Tomato.java
@@ -4,39 +4,41 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 
-class Node{
+class Node {
     private int x;
     private int y;
 
-    Node(int x,int y)
-    {
-        this.x=x;
-        this.y=y;
+    Node(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    int getX(){
+
+    int getX() {
         return this.x;
     }
-    int getY()
-    {
+
+    int getY() {
         return this.y;
     }
 
 }
-class Bfs{
-    int n,m;
-    int cnt=Integer.MIN_VALUE;
+
+class Bfs {
+    int n, m;
+    int cnt = Integer.MIN_VALUE;
     int[][] arr;
-    int[] nx = {-1,1,0,0};
-    int[] ny = {0,0,-1,1};
-    Bfs(int n,int m,int[][] arr)
-    {
+    int[] nx = {-1, 1, 0, 0};
+    int[] ny = {0, 0, -1, 1};
+
+    Bfs(int n, int m, int[][] arr) {
         this.n = n;
         this.m = m;
         this.arr = arr;
     }
+
     public int bfs() {
-        while (!Main.q.isEmpty()) {
-            Node node = Main.q.poll();
+        while (!B7576_Tomato.q.isEmpty()) {
+            Node node = B7576_Tomato.q.poll();
 
             int x = node.getX();
             int y = node.getY();
@@ -47,44 +49,43 @@ class Bfs{
 
                 if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
                     if (arr[nx][ny] == 0) {
-                        Main.q.add(new Node(nx, ny));
-                        arr[nx][ny] = arr[x][y] + 1;
+                        B7576_Tomato.q.add(new Node(nx, ny));                           //익을 수 있는 토마토라면 갯수를 증가시킨다.
+                        arr[nx][ny] = arr[x][y] + 1;                                    //그리고 그노드정보를 큐에 넣는다
                     }
                 }
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(arr[i][j]==0)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0)                                                     //하나라도 0이 있으면 익지 않는다는 것이기 때문에 0일경우는 -1로 출력하여 익지 않는다고 알려준다.
                     return -1;
-                cnt = Math.max(cnt,arr[i][j]);
+                cnt = Math.max(cnt, arr[i][j]);                                         //카운트에는 가진값이 최소경로를 출력해줘야 하기 때문이다.
             }
         }
-        if(cnt==1)
+        if (cnt == 1)
             return 0;
-        return cnt-1;
+        return cnt - 1;
     }
 }
+
 public class B7576_Tomato {
     static Queue<Node> q = new LinkedList<Node>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
         int[][] arr = new int[n][m];
-        for(int i=0;i<n;i++)
-        {
-            st = new StringTokenizer(bf.readLine()," ");
-            for(int j=0;j<m;j++) {
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine(), " ");
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j]==1)
-                    q.add(new Node(i,j));
+                if (arr[i][j] == 1)                                                    //익어있는 토마토정보(노드)를 큐에 저장
+                    q.add(new Node(i, j));
             }
         }
-        Bfs bfs = new Bfs(n,m,arr);
+        Bfs bfs = new Bfs(n, m, arr);
         System.out.println(bfs.bfs());
     }
 }
